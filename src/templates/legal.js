1ef195baadece90dac02f86b1f2f4939a50168ac
Sun Howwrongbum <sun@libermatic.com>
2018-05-14 06:36:09 +0530

    Create template to handle legal pages

---
+++ src/templates/legal.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { Container, Hero, HeroBody, Title, Subtitle, Content } from 'bloomer';
+
+import styles from './legal.module.scss';
+
+const Legal = ({ data }) => {
+  return (
+    <div>
+      <Helmet
+        title={`libermatic. - ${data.markdownRemark.frontmatter.title}`}
+      />
+      <Hero isColor="primary">
+        <HeroBody>
+          <Container>
+            <Title isSize={2}>{data.markdownRemark.frontmatter.title}</Title>
+            <Subtitle>{data.markdownRemark.frontmatter.subtitle}</Subtitle>
+          </Container>
+        </HeroBody>
+      </Hero>
+      <Container>
+        <Content
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+        />
+      </Container>
+    </div>
+  );
+};
+
+export default Legal;
+Legal.propTypes = {
+  data: PropTypes.shape({
+    html: PropTypes.string,
+  }),
+};
+
+export const query = graphql`
+  query pageBy($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      id
+      html
+      frontmatter {
+        title
+        subtitle
+      }
+    }
+  }
+`;
