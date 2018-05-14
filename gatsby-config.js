c959d247b700b1ea0f1ada763f7f95b2f53129e2
Sun Howwrongbum <sun@libermatic.com>
2018-05-14 06:34:31 +0530

    Add dependencies to handle markdown templates

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,14 @@ module.exports = {
     subtitle: 'Open sourcing humans',
   },
   plugins: [
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/data`,
+      },
+    },
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-plugin-postcss-sass',
