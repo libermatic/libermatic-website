9fe726c55fccb1a828fe49e51cf29c8ece24db2b
Sun Howwrongbum <sun@libermatic.com>
2018-05-17 09:45:34 +0530

    Add stub for page /blog

---
+++ src/pages/blog.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import { Hero, HeroBody, Container, Title, Content } from 'bloomer';
+
+import styles from './blog.module.scss';
+
+const Blog = () => (
+  <div>
+    <Helmet title="libermatic. - Blog" />
+    <Hero isColor="dark">
+      <HeroBody>
+        <Container>
+          <Title>Blog</Title>
+        </Container>
+      </HeroBody>
+    </Hero>
+    <Container className={`${styles.section} ${styles.fill}`}>
+      <Content>
+        <p>
+          Later on, here you can read news about <em>libermatic.</em> and other
+          stuffs.
+        </p>
+      </Content>
+    </Container>
+  </div>
+);
+
+export default Blog;
