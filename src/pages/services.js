409145688b7eef8bddf24534db496f37b5d16f0c
Sun Howwrongbum <sun@libermatic.com>
2018-05-18 11:59:54 +0530

    Add page for /services

---
+++ src/pages/services.js
@@ -0,0 +1,115 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import Link from 'gatsby-link';
+import {
+  Hero,
+  HeroBody,
+  Container,
+  Title,
+  Content,
+  Button,
+  Columns,
+  Column,
+  Icon,
+} from 'bloomer';
+
+import styles from './services.module.scss';
+
+const services = [
+  {
+    title: 'Implementation',
+    description: 'Full support and integration with your organization',
+    icon: 'cogs',
+  },
+  {
+    title: 'Training',
+    description: 'Hands on personalized experience for your employees',
+    icon: 'chalkboard-teacher',
+  },
+  {
+    title: 'Customization',
+    description: 'Customize ERPNext according to your requirements',
+    icon: 'wrench',
+  },
+  {
+    title: 'Feature Development',
+    description: 'Build and enhance ERPNext with features not present in core',
+    icon: 'code',
+  },
+];
+
+const Services = () => (
+  <div>
+    <Helmet title="libermatic. - Services" />
+    <Hero isColor="dark">
+      <HeroBody>
+        <Container>
+          <Title>Services</Title>
+        </Container>
+      </HeroBody>
+    </Hero>
+    <Container className={styles.section}>
+      <Content>
+        <p>
+          We provide cloud service and support to easily manage your business.
+          The service we provide would be over ERPNext, a multi purpose business
+          software with features including but not limited to billing, invoicing
+          and inventory management, supported out of the box.
+        </p>
+      </Content>
+      <Button
+        isColor="info"
+        to="/services/erpnext/"
+        render={props => <Link {...props} />}
+      >
+        Learn More
+      </Button>
+    </Container>
+    <Container className={styles.section}>
+      <Columns>
+        <Column isSize="1/4">
+          <Content>
+            <h3>Supports we provide</h3>
+          </Content>
+        </Column>
+        <Column className={styles.content}>
+          <Content>
+            <p>
+              Apart from the ERPNext hosting, we also various other services
+              related to it. Although ERPNext is quite intuitive and easy to
+              use, we can assist you during various stages of its usage. We
+              think you should be able to use the full potential of this
+              application.
+            </p>
+            <p>
+              These services are sometimes clumped together in the packages we
+              offer.
+            </p>
+          </Content>
+          <Button
+            isColor="primary"
+            to="/services/pricing/"
+            render={props => <Link {...props} />}
+          >
+            See Pricing
+          </Button>
+        </Column>
+      </Columns>
+    </Container>
+    <Container className={styles.section}>
+      <div className={styles.pointers}>
+        {services.map(({ icon, title, description }) => (
+          <div key={title}>
+            <Icon className={`fas fa-${icon} fa-3x ${styles.icon}`} />
+            <dl>
+              <dt>{title}</dt>
+              <dd>{description}</dd>
+            </dl>
+          </div>
+        ))}
+      </div>
+    </Container>
+  </div>
+);
+
+export default Services;
