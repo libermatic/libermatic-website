556d123b34e603b7ec53261862001ccc0d4cbb09
Sun Howwrongbum <sun@libermatic.com>
2018-05-19 05:47:50 +0530

    Contact: Update email regex

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -26,7 +26,7 @@ class Contact extends Component {
   isValid = () => {
     const { email } = this.state.fields;
     if (email) {
-      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
       return re.test(String(email).toLowerCase());
     }
     return false;
