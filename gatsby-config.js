1e49bc25b372e31c84a3090610caad68021deef7
Sun Howwrongbum <sun@libermatic.com>
2018-05-19 05:46:05 +0530

    Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,10 @@ module.exports = {
   siteMetadata: {
     title: 'libermatic.',
     subtitle: 'Open sourcing humans',
+    siteUrl: 'https://libermatic.com',
   },
   plugins: [
+    'gatsby-plugin-sitemap',
     'gatsby-transformer-remark',
     {
       resolve: 'gatsby-source-filesystem',
